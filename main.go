a1b16eda5154b16ec4f59fc3e4433d3fff9dd8c4
Jose A. Delgado <jpenaroche@gmail.com>
2022-01-04 12:17:07 -0300

    chore: move routes to server initialization

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/jpenaroche/go-simple-api/api/routes"
-
 func main() {
-	Run(routes.Routes)
+	Run() //TODO inject Context
 }
--- server.go
+++ server.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/jpenaroche/go-simple-api/api/routes"
 	"github.com/jpenaroche/go-simple-api/utils"
 )
 
 const port = "8080"
 const server = "localhost"
 
-func Run(modules map[string][]utils.RouteParameter) {
-	handler := new(utils.Router).ProcessRoutes(modules)
+func Run() {
+	handler := new(utils.Router).ProcessRoutes(routes.Routes)
+	log.Println("Server running on http://" + server + ":" + port)
 	_ = http.ListenAndServe(fmt.Sprintf("%s:%s", server, port), handler)
 }
